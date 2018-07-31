67cd2b86b2046d488b6687597aefc7fcf92c8902
wangzl8@lenovo.com <wangzl8@lenovo.com>
2018-07-31 23:11:22 +0800

    修正最后一个错位显示的bug

--- src/_8x8x8_lue_LED_cube_V1.00/ScreenFunction.ino
+++ src/_8x8x8_lue_LED_cube_V1.00/ScreenFunction.ino
@@ -6,18 +6,66 @@ volatile int8_t screen[28][3] = {
   {7, 7}, {6, 7}, {5, 7}, {4, 7}, {3, 7}, {2, 7}, {1, 7},
 };
 volatile int8_t content[] = {
-  B11111111,
-  B10111110,
-  B00000000,
-  B11111110,
-  B11111111
+//  B11111111,
+//  B10111110,
+//  B00000000,
+//  B11111110,
+//  B11111111,
+//  
+//  B11111111,
+//  B10111100,
+//  B01111010,
+//  B01110110,
+//  B10001110,
+//  
+//  B11111111,
+//  B10111101,
+//  B01101110,
+//  B01100110,
+//  B10011001,
+//
+//  B11111111,
+//  B11100011,
+//  B11011011,
+//  B00000000,
+//  B11111011,
+
+//  B11111111,
+//  B10001101,
+//  B01101110,
+//  B01101110,
+//  B01110001,
+
+    B11111111,
+    B10000001,
+    B01101110,
+    B01101110,
+    B10110001,
+    
+    B11111111,
+    B10010001,
+    B01101110,
+    B01101110,
+    B10010001,
+    
+    B11111111,
+    B10011101,
+    B01101110,
+    B01101110,
+    B10000001,
+    
+    B11111111,
+    B10000001,
+    B01111110,
+    B01111110,
+    B10000001
 };
 
 int content_len;
 
 void Screen01() {
   int yy, zz, suby, subz;
-  int m, o, p = 0, q;
+  int m, o, p = 0, q, r;
   content_len = sizeof(content) / sizeof(content[0]);
   for (int n = 0; n <= SCREEN_LENGTH + content_len; n++) {
     o = n;
@@ -32,8 +80,10 @@ void Screen01() {
       for (; o >= 0; o--) {
         yy = screen[o][0];
         zz = screen[o][1];
-        cube[yy][zz] = content[p];
-        p++;
+        if (p < content_len) {
+          cube[yy][zz] = content[p];
+          p++;
+        }
       }
     } else if (o <= SCREEN_LENGTH) {
       q = o - content_len;
@@ -45,13 +95,16 @@ void Screen01() {
         p++;
       }
     } else {
-      o = o - content_len;
+      r = o - SCREEN_LENGTH - 1;
+      o = o - content_len + 1;
       p = content_len - 1;
       for (; o < SCREEN_LENGTH; o++) {
         yy = screen[o][0];
         zz = screen[o][1];
-        cube[yy][zz] = content[p];
-        p--;
+        if (p > r) {
+          cube[yy][zz] = content[p];
+          p--;
+        }
       }
     }
     delay(100);
